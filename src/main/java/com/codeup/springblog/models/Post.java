e2c41857fe21f171665e8d48f00344d5acbb342a
Andrew Brought <andrewbrought@gmail.com>
2020-08-03 15:25:22 -0500

    exercise walkthrough

---
+++ src/main/java/com/codeup/springblog/models/Post.java
@@ -0,0 +1,39 @@
+package com.codeup.springblog.models;
+
+public class Post {
+    private String title;
+    private String body;
+    private long id;
+
+    public Post() {}
+
+    public Post(long id, String title, String body) {
+        this.id = id;
+        this.title = title;
+        this.body = body;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String newTitle) {
+        this.title = newTitle;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String newBody) {
+        this.body = newBody;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
