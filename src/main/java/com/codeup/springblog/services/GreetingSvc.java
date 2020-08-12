f16f33b8295ca5301afbb425920a2789ec4ea726
Andrew Brought <andrewbrought@gmail.com>
2020-08-12 15:55:03 -0500

    security walkthrough

--- src/main/java/com/codeup/springblog/services/GreetingSvc.java
+++ src/main/java/com/codeup/springblog/services/GreetingSvc.java
@@ -1,4 +1,21 @@
 package com.codeup.springblog.services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class GreetingSvc {
+//    Want this to have a bunch of methods that take in a name and return a greeting...
+
+    public String goodMorning(String name) {
+        return "Good morning, " + name + "!";
+    }
+
+    public String goodAfternoon(String name) {
+        return "Good afternoon, " + name + "!";
+    }
+
+    public String goodEvening(String name) {
+        return "Good evening, " + name + "!";
+    }
+
 }
